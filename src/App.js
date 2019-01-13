29b547efdf81e1b20e14c06c2ed6bab0351dab46
Said Maadan <me@saidmaadan.com>
2019-01-13 01:01:53 -0600

    Designed navbar and open-sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './static/styles.css';
+import Header from './components/header/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Hello</h2>
+      <div style={{ height:"1500px",background:'cornflowerblue'}}>
+        <Header/>
       </div>
     );
   }
---
+++ src/components/header/SideDrawer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { scroller } from 'react-scroll';
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideDrawer = (props) => {
+
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element,{
+            duration: 1500,
+            delay:100,
+            smooth: true,
+            offset: -150
+        });
+        props.onClose(false)
+    }
+    return (
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=> props.onClose(false)}
+        >
+            <List component="nav">
+                <ListItem button onClick={()=> scrollToElement('home')}>
+                    Home
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('about')}>
+                    About
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('projects')}>
+                    Projects
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('tech-stack')}>
+                    Tech-Stack
+                </ListItem>
+
+                <ListItem button onClick={()=> scrollToElement('contact')}>
+                    Contact
+                </ListItem>
+
+            </List> 
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
